1f7b60bf87762eb18c7212bd2d3a88830c4beaa0
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2016-08-21 20:30:24 -0300

    Fix icon y config descripcion

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $http) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -14,41 +14,60 @@ angular.module('starter.controllers', [])
 
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
+	scope: $scope
   }).then(function(modal) {
-    $scope.modal = modal;
+	$scope.modal = modal;
   });
 
   // Triggered in the login modal to close it
   $scope.closeLogin = function() {
-    $scope.modal.hide();
+	$scope.modal.hide();
   };
 
   // Open the login modal
   $scope.login = function() {
-    $scope.modal.show();
+	$scope.modal.show();
   };
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
+	// console.log('Doing login', $scope.loginData);
+	// // Simulate a login delay. Remove this and replace with your login
+	// // code if using a login system
+	// $timeout(function() {
+	//   $scope.closeLogin();
+	// }, 1000);
+	var data = {
+	  "user": {
+		"email":  $scope.loginData.username,
+		"password":  $scope.loginData.password
+	  }
+	}
+	$http({
+		url: "http://app.rizoma.io/users/sign_in",
+		method: "POST",
+		data: data
+	}).success(function(data, status, headers, config) {
+		alert(status);
+		alert(headers);
+		$scope.data = data;
+		$scope.session = data;
+		$scope.closeLogin();
+	}).error(function(data, status, headers, config) {
+		$scope.status = status;
+		alert("No fue posible autenticarte.");
+	});
   };
 })
 
 .controller('ScansCtrl', function($scope) {
   $scope.scans = [
-    { title: 'Cargamento palta', id: 1 },
-    { title: 'Cargamento manzana', id: 2 },
-    { title: 'El floral', id: 3 },
-    { title: 'Mingus', id: 4 },
-    { title: 'Cargamento x', id: 5 },
-    { title: 'Cargamento xxx', id: 6 }
+	{ title: 'Cargamento palta', id: 1 },
+	{ title: 'Cargamento manzana', id: 2 },
+	{ title: 'El floral', id: 3 },
+	{ title: 'Mingus', id: 4 },
+	{ title: 'Cargamento x', id: 5 },
+	{ title: 'Cargamento xxx', id: 6 }
   ];
 })
 
@@ -67,17 +86,17 @@ angular.module('starter.controllers', [])
 })
 
 .controller("NewScanCtrl", function($scope, $cordovaBarcodeScanner) {
-    $scope.codes = []
-    $scope.scanBarcode = function() {
-        $cordovaBarcodeScanner.scan().then(function(imageData) {
-            $scope.codes.push(imageData.text);
-            // alert(imageData.text);
-            // console.log("Barcode Format -> " + imageData.format);
-            // console.log("Cancelled -> " + imageData.cancelled);
-        }, function(error) {
-            // console.log("An error happened -> " + error);
-        });
-    };
+	$scope.codes = []
+	$scope.scanBarcode = function() {
+		$cordovaBarcodeScanner.scan().then(function(imageData) {
+			$scope.codes.push(imageData.text);
+			// alert(imageData.text);
+			// console.log("Barcode Format -> " + imageData.format);
+			// console.log("Cancelled -> " + imageData.cancelled);
+		}, function(error) {
+			// console.log("An error happened -> " + error);
+		});
+	};
 });
 
 // .controller('ScanCtrl', function($scope, $stateParams) {
