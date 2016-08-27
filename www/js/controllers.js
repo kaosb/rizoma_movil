0e73bf7f0d5dad9d24e098aee13ffa5fc54c027f
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2016-08-27 19:30:13 -0300

    Avances respecto a session

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,81 +1,76 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout, $http) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $http, $sessionStorage) {
+// With the new view caching in Ionic, Controllers are only called
+// when they are recreated or on app start, instead of every page change.
+// To listen for when this page is active (for example, to refresh data),
+// listen for the $ionicView.enter event:
+//$scope.$on('$ionicView.enter', function(e) {
+//});
+  
+	// Construyo el elemento que mantendra a la session.
+	$scope.session = $sessionStorage;
+  
+	// Datos de formulario para el formulario de login.
+	$scope.loginData = {};
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-	scope: $scope
-  }).then(function(modal) {
-	$scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-	$scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-	$scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-	// console.log('Doing login', $scope.loginData);
-	// // Simulate a login delay. Remove this and replace with your login
-	// // code if using a login system
-	// $timeout(function() {
-	//   $scope.closeLogin();
-	// }, 1000);
-	var data = {
-		"email": $scope.loginData.username,
-		"password": $scope.loginData.password
-	}
-	$http({
-		url: "http://app.rizoma.io/api/v1/tokens.json",
-		method: "POST",
-		data: data
-	}).success(function(data, status, headers, config) {
-		alert(status);
-		alert(data.token);
-		$scope.data = data.token;
-		$scope.session = data.token;
-		$scope.closeLogin();
-	}).error(function(data, status, headers, config) {
-		$scope.status = status;
-		alert("No fue posible autenticarte.");
+	// Crea el modal de login, que posteriormente se usa.
+	$ionicModal.fromTemplateUrl('templates/login.html', {
+		scope: $scope
+	}).then(function(modal) {
+		$scope.modal = modal;
 	});
-  };
 
+	// Oculta el modal de login.
+	$scope.closeLogin = function() {
+		$scope.modal.hide();
+	};
 
-  $scope.getClients = function(){
-  	if($scope.session){
-	  	$http({
-			url: "http://app.rizoma.io/api/v1/clients.json",
-			method: "GET",
-			data: {'auth_token': $scope.session}
-		}).success(function(data, status) {
-			alert(status);
-			alert(data.session);
-			$scope.scans = data;
-			return data;
+	// Abre el modal de login.
+	$scope.login = function() {
+		$scope.modal.show();
+	};
+
+	// Ejecuta el login, contra el endpoint con la informacion del formulario.
+	$scope.doLogin = function() {
+		var data = {
+			"email": $scope.loginData.username,
+			"password": $scope.loginData.password
+		}
+		$http({
+			url: "http://app.rizoma.io/api/v1/tokens.json",
+			method: "POST",
+			data: data
+		}).success(function(data, status, headers, config) {
+			$scope.session.status = status;
+			$scope.session.token = data.token;
+			alert($scope.session);
+			$scope.closeLogin();
 		}).error(function(data, status, headers, config) {
-			$scope.status = status;
-			alert("No fue posible obtener los clientes.");
-			return null;
+			$scope.session.status = status;
+			alert("No fue posible autenticarte.");
 		});
-  	}
-  };
+	};
+
+	// Obtiene los clientes contra el endpoint de rizoma.
+	$scope.getClients = function(){
+		if($scope.session){
+			$http({
+				url: "http://app.rizoma.io/api/v1/clients.json",
+				method: "GET",
+				data: {'auth_token': $scope.session}
+			}).success(function(data, status) {
+				alert(status);
+				alert(data.session);
+				$scope.scans = data;
+				return data;
+			}).error(function(data, status, headers, config) {
+				$scope.status = status;
+				alert("No fue posible obtener los clientes.");
+				return null;
+			});
+		}
+	};
 
 })
 
