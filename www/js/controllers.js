a095e19ec289caabfd7855747355ad8de10c95f3
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2016-08-28 06:15:35 -0300

    Avances

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -43,8 +43,7 @@ angular.module('starter.controllers', [])
 			data: data
 		}).success(function(data, status, headers, config) {
 			$scope.session.status = status;
-			// $scope.session.token = data.token;
-			$scope.session.token = "kyqwXT61Hys5xhcd6A6U";
+			$scope.session.token = data.token;
 			$scope.closeLogin();
 		}).error(function(data, status, headers, config) {
 			console.log("No fue posible autenticarte.");
@@ -53,7 +52,7 @@ angular.module('starter.controllers', [])
 
 	// Obtiene los clientes contra el endpoint de rizoma.
 	$scope.getClients = function(){
-		$scope.session.token = "kyqwXT61Hys5xhcd6A6U";
+		console.log("getClients -> " + $scope.session.token);
 		$http({
 			url: "http://app.rizoma.io/api/v1/clients.json?auth_token="+$scope.session.token,
 			method: "GET"
@@ -69,12 +68,11 @@ angular.module('starter.controllers', [])
 	// Verificar Session.
 	$scope.checkSession = function(){
 		if($scope.session.hasOwnProperty("token") && $scope.session.status == 200){
-			console.log("Tu token es: "+$scope.session.token);
+			console.log("Tu token es: " + $scope.session.token);
 			return true;
 		}else{
 			console.log("La session no existe, es necesario autenticar.");
 			// $scope.login();
-			// $location.path( "/login" );
 			return false;
 		}
 	};
@@ -89,7 +87,7 @@ angular.module('starter.controllers', [])
 
 .controller("NewScanCtrl", function($scope, $ionicModal, $cordovaBarcodeScanner){
 	// Chequeo la session.
-	// $scope.checkSession();
+	$scope.checkSession();
 	// Ejecuto la magia.
 	
 	// Array que mantiene los codigos.
@@ -98,7 +96,6 @@ angular.module('starter.controllers', [])
 	// Funcion responsable de lanzar el escaner.
 	$scope.scanBarcode = function(){
 		$cordovaBarcodeScanner.scan().then(function(imageData){
-			$scope.session.token = "kyqwXT61Hys5xhcd6A6U";
 			// Verifico si el codigo es valido.
 			$http({
 				url: "http://app.rizoma.io/api/v1/item_group/"+imageData.text+".json?auth_token="+$scope.session.token,
@@ -116,7 +113,6 @@ angular.module('starter.controllers', [])
 
 	// Funcion responsable de enviar los codigos al servidor.
 	$scope.sendCodes = function(){
-		$scope.session.token = "kyqwXT61Hys5xhcd6A6U";
 		var data = {
 			"auth_token":$scope.session.token,
 			"transaction_client": "57b9f21a861ca36309000003",
@@ -136,26 +132,54 @@ angular.module('starter.controllers', [])
 
 .controller("ScanCtrl", function($scope, $ionicModal, $cordovaBarcodeScanner){
 	// Chequeo la session.
-	// $scope.checkSession();
+	$scope.checkSession();
 	// Ejecuto la magia.
+	
+	// Array que mantiene los codigos.
 	$scope.codes = []
-	$scope.scanBarcode = function() {
+
+	// Funcion responsable de lanzar el escaner.
+	$scope.scanBarcode = function(){
 		$cordovaBarcodeScanner.scan().then(function(imageData){
-			$scope.codes.push(imageData.text);
-			// alert(imageData.text);
-			// console.log("Barcode Format -> " + imageData.format);
-			// console.log("Cancelled -> " + imageData.cancelled);
+			// Verifico si el codigo es valido.
+			$http({
+				url: "http://app.rizoma.io/api/v1/item_group/"+imageData.text+".json?auth_token="+$scope.session.token,
+				method: "GET"
+			}).success(function(data, status){
+				// Al ser valido lo agrego como positivo.
+				$scope.codes.push(imageData.text);
+			}).error(function(data, status, headers, config){
+				console.log("No fue posible validar el codigo capturado.");
+			});
 		}, function(error) {
 			// console.log("An error happened -> " + error);
 		});
 	};
+
+	// Funcion responsable de enviar los codigos al servidor.
+	$scope.sendCodes = function(){
+		var data = {
+			"auth_token":$scope.session.token,
+			"transaction_client": "57b9f21a861ca36309000003",
+			"transaction_items_groups": ['57b9f3ef861ca36309000019','57bf3e4f861ca36195000006','57bf3e5f861ca36195000009']
+		}
+		$http({
+			url: "http://app.rizoma.io/api/v1/transactions",
+			method: "POST",
+			data: data
+		}).success(function(data, status, headers, config) {
+			console.log("Se envio de forma exitosa la carga: "+data);
+		}).error(function(data, status, headers, config) {
+			console.log("No se puedo enviar la carga..");
+		});
+	}
 })
 
 .controller('ClientsCtrl', function($scope, $ionicModal, $stateParams) {
 	// Chequeo la session.
 	// $scope.checkSession();
 	// Ejecuto la magia.
-	$scope.getClients();
+	// $scope.getClients();
 	if($scope.session && $scope.session.clients){
 		$scope.getClients();
 		$scope.clients = Array();
